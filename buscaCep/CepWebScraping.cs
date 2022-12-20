a97c1b1cb7f1a47ccda4e8a9a715808a92e2ed6a
RanimyLeite <ranimy.leitecosta@gmail.com>
2022-12-20 11:43:41 -0300

    Adicionar arquivos de projeto.

---
+++ buscaCep/CepWebScraping.cs
@@ -0,0 +1,40 @@
+﻿using buscaCep.DTOs;
+using System;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using static System.Net.WebRequestMethods;
+
+namespace buscaCep
+{
+    public class CepWebScraping
+    {
+        private static HttpClient _httpClient;
+        private static HttpClient HttpClient => _httpClient ?? (_httpClient = new HttpClient());
+
+        public async Task GetCepInfo(string cep)
+        {
+
+            try
+            {
+                var url = "https://" + "viacep.com.br/ws/" + cep +"/json/";
+
+                HttpResponseMessage response = await HttpClient.GetAsync(url);
+
+                if (response.IsSuccessStatusCode)
+                {
+                    string responseBody = await response.Content.ReadAsStringAsync();
+
+                    Console.WriteLine("As informações referentes ao CEP passado, são: ");
+
+                    Console.WriteLine(responseBody);
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+            
+        }
+    }
+}
---
+++ buscaCep/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace buscaCep
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine();
+            Console.WriteLine("Entre com a informações de CEP");
+            Console.WriteLine();
+            Console.WriteLine();
+
+            Console.Write("Entre com o seu CEP: ");
+            var cep = Console.ReadLine();
+            Console.WriteLine();
+
+            var cepInfo = new CepWebScraping();
+            var getCep = cepInfo.GetCepInfo(cep);
+
+            Console.ReadKey();
+        }
+    }
+}
